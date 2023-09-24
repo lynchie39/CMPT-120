6c065cd5d25a4357e4a92832afb2ca442698e6e6
lynchie39 <cmlynch2305@gmail.com>
2023-09-24 18:11:45 -0400

    x

--- Assignment 2/if_else_input_practice.py
+++ Assignment 2/if_else_input_practice.py
@@ -10,4 +10,15 @@
 #additional instructions: instead of setting our variable to 10, what if we asked the user for the variable and used that value in our comparisons?
 def main():
 
+    Var = input("Please write any number: ")
+    Var = int(Var)
+    if Var > 12:
+        print("Var is greater than 12")
+    elif Var > 11:
+        print("Var is greater than 11")
+    elif Var == 10:
+        print("Var is equal to 10")
+    else:
+        print("Var is less than 10")
+
 main()
