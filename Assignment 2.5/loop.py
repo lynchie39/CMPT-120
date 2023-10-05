a1e17e2caea32eff072cdd0e3a2684cd97c90f96
lynchie39 <cmlynch2305@gmail.com>
2023-10-05 08:06:54 -0400

    s

---
+++ Assignment 2.5/loop.py
@@ -0,0 +1,16 @@
+name = "chris"
+for x in (name):
+    print(name)
+namelet = ["c", "h", "r", "i", "s"]
+for x in namelet:
+    print(x)
+
+i = 0
+while i < 6:
+    print(i)
+    i = i + 1
+
+b = 0
+while b < 4:
+    print(name)
+    b = b + 1
