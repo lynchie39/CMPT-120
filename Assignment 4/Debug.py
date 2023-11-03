5db6baa3a84dfb445661d6bc233c6e4777ff5899
lynchie39 <cmlynch2305@gmail.com>
2023-11-03 12:39:32 -0400

    Update Debug.py

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -7,9 +7,16 @@ def printName(x):
     
 def addition(x, y):
     #add x and y together and return them
-    return
+    z = x + y
+    return z
 
 def smaller(i, j):
+    if i < j:
+        return i
+    elif j < i:
+        return j
+    else:
+        return 0
     #if i is smaller than j, return i
     #if j is smaller than i, return j
     #if they're even, return 0
@@ -17,18 +24,19 @@ def smaller(i, j):
 def main():
     
     #call the printHello function here
+    print(printHello())
     
     #call printName and give it the parameter of your name
     
     var1= 10
     var2= 20
     #What do we put in here to make it work?
-    print(addition())
+    print(addition(10, 20))
     
-    num1 = int(input("Enter number 1"))
-    num2 = int(input("Enter number 2"))
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
     #what go here?
-    print(smaller())
+    print(smaller(num1, num2))
 
 
 
