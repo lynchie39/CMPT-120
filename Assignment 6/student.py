0297e393ca998edc06a28e15839ce14f50dd2f2d
lynchie39 <cmlynch2305@gmail.com>
2023-11-09 00:43:24 -0500

    Update student.py

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -24,15 +24,42 @@ create a function because this college is a wacky one- every day they generate a
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
 
-class student:
+import random
+
+class Student:
+    def __init__(self, name, studentid, year, major, gpa):
+        self.name = name
+        self.studentid = studentid
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def honorseligible(self):
+        if self.gpa > 3.5:
+
+            print(self.name, "Is eligible for the honors program!")
+            return True
+        else:
+            print(self.name, "Is not eligible for the honors program.")
+            return False
+
+    def freelunch(self):
+        randomid = random.randint(1000, 9999)
+        print(randomid)
+        if self.studentid == randomid:
+            print("Winner! student", self.name, "gets free lunch")
+        else:
+            print("Loser!")
+
     
     
-    
-    
+
     
     
 def main():
     #create three students and check if they get free lunch and if they qualify for honors
-    
+    student = Student("Chris", 6969, "Freshman", "Computer Science", 3.7)
+    student.honorseligible()
+    student.freelunch()
     
 main()
